c48f6c23b7185f34e9a77dfabf390adc2a84cd01
420087896@pcpuma.acatlan.unam.mx <420087896@pcpuma.acatlan.unam.mx>
2022-05-19 00:25:31 -0500

    codigosPostal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import React from 'react';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import Register from './Register';
+
 
 function App() {
-  const [contador,setContador]=useState(0);
-  const updateData=()=>{
-    try{
-      setContador(parseInt(contador)+1);
-    }catch (error){
-      console.log(error);
-    }
-  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Contador : {contador}. <br></br>
-          <button onClick={updateData}>Actualizar</button>
-        </p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Register} exact /> 
+          </Switch>
+        </BrowserRouter>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Register.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+import swal from 'sweetalert';
+import * as toastr from 'toastr';
+import 'toastr/build/toastr.css';
+import React, { useState } from 'react';
+
+
+
+function Register (){
+    const [cod,updcod]=useState("");
+    const updateData=()=>{
+        try{
+            updcod =updateData;
+        }catch (error){
+            console.log(error);
+        }
+    }
+    const getCP = async () =>{
+        try {
+
+            let formData = new FormData();
+            formData.append("cp",cod);
+            const url = "http://api.masksoftco.mx/direcciones/codigo-postal";
+
+            let result = await axios({
+                url,
+                method: 'POST',
+                dataType: 'json',
+                ContentType: 'application/json',
+                data: formData
+            });
+            if(result.data){
+                let objectRes = result.data;
+                for(let i=0; i<objectRes.length; i++){
+                    console.log(objectRes[i].colonia);
+                }
+                toastr.success("Información consultada", "correcto")
+            }else{
+                swal("Opps!", "Código postal incorrecto", "error");
+            }
+
+
+            console.log(result)
+            console.log(result.data)
+            
+
+
+           // return (res.data ? res.data : false);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return(
+        <div className="content">
+            <form>
+                <div class="row">
+                    <div class="col-md-3">
+                        <label for="cd">CodigoPostal</label>
+                        <input type="text" class="form-control" id="cd" placeholder="Codigo Postal" onChange={(e)=>updateData(e.target.value)} />
+                    </div>
+                    <div class="col-md-3" >
+                        <label for="est">Estado</label>
+                        <select type="text" class="form-control" id="est" placeholder="--Selecione--" disabled>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="mun">Municipio</label>
+                        <select type="text" class="form-control" id="mun" placeholder="--Selecione--"disabled>
+                            <option>--Seleccione--</option>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="col">Colonia</label>
+                        <select type="text" class="form-control" id="col" placeholder="Selecione">
+                            <option>--Seleccione--</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+            <button onClick={getCP}>Obtener</button>
+        </div>
+    );
+
+}
+export default Register;
