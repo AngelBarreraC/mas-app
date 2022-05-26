e01b77b354f51857759cfbf0641ba5165ec4756f
420087896@pcpuma.acatlan.unam.mx <420087896@pcpuma.acatlan.unam.mx>
2022-05-25 23:09:35 -0500

    consulta

--- src/Consult.js
+++ src/Consult.js
@@ -1,33 +1,53 @@
-import React,{useEffect,useState}  from "react";
 import axios from "axios";
-import swal from 'sweetalert';
-import * as toastr from 'toastr';
 import 'toastr/build/toastr.css';
+import React, { useState } from 'react';
 
 function Consult (){
-    const url='https://apigrupogr.com/grapi/clientes/lista-clientes'
-    const[todos,setTodos]=useState()
-    const [action,runAction]=useState(0)
-    const setAction=()=>{
-        runAction(1);
-    }
+    const[todos,setTodos]=useState([]);
     const getClientes = async () =>{
-        const response=await fetch(url)
-        const responseJson=await response.json();
-        setTodos(responseJson)
+        try{
+            const url = " https://apigrupogr.com/grapi/clientes/lista-clientes"
+            let result=await axios({
+                url,
+                method: 'GET',
+                dataType : 'json',
+                ContentType: 'application/json',
+            });
+            setTodos( result.data.clients);
+        }catch(error){
+            alert(error);
+        }
     }
-    useEffect(()=>{
-        getClientes()
-
-    },[setAction])
 
     return(
-        <div className="content">
+        <div className="container App">
             <button onClick={getClientes}>CONSULTAR</button><tr></tr>
-            { !todos?  'Cargando...':
-              todos.map(()=>{
-                  return <li>{todos}</li>
-              })}
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col"> ID </th>
+                        <th scope="col"> Nombre</th>
+                        <th scope="col"> Descripción</th>
+                        <th scope="col"> Correo</th>
+                        <th scope="col"> Regimen Fiscal </th>
+                        <th scope="col"> RFC</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    todos.map( (data)=> 
+                    <tr key={"celda"+data.id_cliente}>
+                        <td key={"id_cliente"+data.id_cliente}>{data.id_cliente}</td> 
+                        <td key={"nombre"+data.id_cliente}>{data.nombre}</td>
+                        <td key={"descripcion"+data.descripcion}>{data.descripcion} </td>
+                        <td key={"correo"+data.correo}>{data.correo}</td>
+                        <td key={"reg_fiscal"+data.reg_fiscal}>{data.reg_fiscal}</td>
+                        <td key={"rfc"+data.urf}>{data.rfc}</td>
+                    </tr>
+                    )
+                }
+                </tbody>
+            </table> 
         </div>
     );
 
